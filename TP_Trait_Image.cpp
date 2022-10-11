5b8a53b9d4cf73b2d3016eb04d617732c09ae1eb
theo.mistral <mistheo73@gmail.com>
2022-10-11 16:21:12 +0200

    Configuration des propriété du projet avec le repertoire des include et l'ajout des librairies

--- TP_Trait_Image.cpp
+++ TP_Trait_Image.cpp
@@ -1,20 +1,39 @@
-// TP_Trait_Image.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
+//! [includes]
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 #include <iostream>
 
+using namespace cv;
+//! [includes]
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    //! [imread]
+    std::string image_path = samples::findFile("starry_night.jpg");
+    Mat img = imread(image_path, IMREAD_COLOR);
+    //! [imread]
+
+    //! [empty]
+    if (img.empty())
+    {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
+    }
+    //! [empty]
+
+    //! [imshow]
+    imshow("Display window", img);
+    int k = waitKey(0); // Wait for a keystroke in the window
+    //! [imshow]
+
+    //! [imsave]
+    if (k == 's')
+    {
+        imwrite("starry_night.png", img);
+    }
+    //! [imsave]
+
+    return 0;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
