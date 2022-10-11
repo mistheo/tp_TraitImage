c67f0cee0f5dfac3e9d5abfade199578f7185d79
theo.mistral <mistheo73@gmail.com>
2022-10-11 17:25:36 +0200

    Code qui permet de donnée une image en entrée et qui resort une image en verison grisé

--- TP_Trait_Image.cpp
+++ TP_Trait_Image.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
 #include <iostream>
@@ -10,16 +11,25 @@ int main()
 {
     std::string image_path = samples::findFile("starry_night.jpg");
     Mat img = imread(image_path, IMREAD_COLOR);
+    Mat imgGray = imread("starr_night_gray.jpg", IMREAD_GRAYSCALE);
+
+
     if (img.empty())
     {
         std::cout << "Could not read the image: " << image_path << std::endl;
         return 1;
     }
-    imshow("Display window", img);
+    imshow("Image Origine", img);
+ 
+
+    imgGray *= 1. / 255;
+    cvtColor(img, imgGray, COLOR_RGB2GRAY);
+
+    imshow("Image Grisé", imgGray);
     int k = waitKey(0); // Wait for a keystroke in the window
     if (k == 's')
     {
-        imwrite("starry_night.png", img);
+        imwrite("starry_night.jpg", img);
     }
     return 0;
 }
