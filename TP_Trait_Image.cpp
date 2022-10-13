926f0975ee34656fef89e13b44dd6d01b76373aa
theo.mistral <mistheo73@gmail.com>
2022-10-13 14:20:54 +0200

    Ajout d'une fonction setPixel et d'une fonction setImgInGray

--- TP_Trait_Image.cpp
+++ TP_Trait_Image.cpp
@@ -1,35 +1,57 @@
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
+//Include de OpenCV
+#include <opencv2/core.hpp> // noyaux de OpenCV
+#include <opencv2/imgcodecs.hpp> //images
+#include <opencv2/imgproc.hpp> //traitement d'images
+#include <opencv2/highgui.hpp> //gui
 
+//Include C++
 #include <iostream>
 using namespace cv;
 
+void setPixel(Mat&, int, int, uchar);
+void setImgInGray(Mat&, Mat&);
+const uchar BLANC = 255, NOIRE = 0;
+
 
 int main()
-{
+{  
     std::string image_path = samples::findFile("starry_night.jpg");
-    Mat img = imread(image_path, IMREAD_COLOR);
+    Mat imgOrig = imread(image_path, IMREAD_COLOR);
     Mat imgGray = imread("starr_night_gray.jpg", IMREAD_GRAYSCALE);
 
-
-    if (img.empty())
+    if (imgOrig.empty())//Si le fichier n'existe pas
     {
         std::cout << "Could not read the image: " << image_path << std::endl;
         return 1;
     }
-    imshow("Image Origine", img);
- 
+    
+    setImgInGray(imgOrig, imgGray);
+    imshow("Image Origine ", imgOrig);
+    imshow("Image Grisé ", imgGray);
 
-    imgGray *= 1. / 255;
-    cvtColor(img, imgGray, COLOR_RGB2GRAY);
-
-    imshow("Image Grisé", imgGray);
-    int k = waitKey(0); // Wait for a keystroke in the window
-    if (k == 's')
+    if (waitKey(0) == 's')
     {
-        imwrite("starry_night.jpg", img);
+        imwrite("starry_night.jpg", imgOrig);
     }
     return 0;
+
+}
+
+void setImgInGray(Mat& imgOrig,Mat& imgGray)
+{
+    //imshow("Image Origine ", imgOrig);
+    imgGray *= 1. / 255;
+    cvtColor(imgOrig, imgGray, COLOR_RGB2GRAY); //transformer l'image en grisé
+    //imshow("Image Grisé ", imgGray);
+}
+
+
+
+void setPixel(Mat& image, int i, int j , uchar couleur)
+{
+    // accept only char type matrices
+    CV_Assert(image.depth() == CV_8U);
+    CV_Assert(image.channels() == 1);
+    const int channels = image.channels();
+    if(channels==1) image.at<uchar>(i, j) = couleur;
 }
